52227741a5d5316740059190be1ab3d891199075
user <administrator@Srihari.local>
2024-01-11 17:49:09 +0530

    commited Done

--- app1.py
+++ app1.py
@@ -3,14 +3,14 @@ import instaloader
 L = instaloader.Instaloader()
 
 INSTAGRAM_USERNAME = "loopstar154"
-INSTAGRAM_PASSWORD = "Starbuzz4@"
+INSTAGRAM_PASSWORD = "Starbuzz6@"
 
 # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 # L.context._session.headers["User-Agent"] = user_agent
 
 try:
     L.context.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
-    with open('loopstar154_session2', 'wb') as f:
+    with open('loopstar154_session6', 'wb') as f:
         L.context.save_session_to_file(f)
 except Exception as e:
     print(f"An error occurred: {e}")
