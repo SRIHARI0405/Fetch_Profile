f3e33ace41a1c22b37bec5683b6956784ad10358
user <administrator@Srihari.local>
2024-01-11 22:09:09 +0530

    commited Done

--- app2.py
+++ app2.py
@@ -4,84 +4,50 @@ import instaloader
 import logging
 import os
 import re
+import time
+import random
 
 app = Flask(__name__)
 
-SESSION_FILE = "loopstar154_session8"
-INSTAGRAM_USERNAME = "loopstar154"
-INSTAGRAM_PASSWORD = "Starbuzz6@"
-
-L = instaloader.Instaloader()
-
-
+SESSION_FILE = os.getenv('INSTA_SESSION_FILE', 'loopstar154_session7')
+INSTAGRAM_USERNAME = os.getenv('loopstar154')
+INSTAGRAM_PASSWORD = os.getenv('Starbuzz6@')
+HTTP_PROXY = os.getenv('socks5://yoqytafd-6:2dng483b96qx@p.webshare.io:80')
+HTTPS_PROXY = os.getenv('socks5://yoqytafd-6:2dng483b96qx@p.webshare.io:80')
 
 def create_instaloader_instance():
     USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    L = instaloader.Instaloader()
     L.user_agent = USER_AGENT  
-
-    # try:
-    #     with open(SESSION_FILE, 'rb') as session_file:
-    #         L.context.load_session_from_file(INSTAGRAM_USERNAME,session_file)
-    # except instaloader.exceptions.QueryReturnedNotFoundException:
-    #     L.context.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
-    #     with open(SESSION_FILE, 'wb') as session_file:
-    #         L.context.save_session_to_file(session_file)
-
 
     try:
         if os.path.exists(SESSION_FILE):
-            with open(SESSION_FILE, 'rb') as session_file:
-                L.context.load_session_from_file(INSTAGRAM_USERNAME, session_file)
-            
+            L.load_session_from_file(INSTAGRAM_USERNAME, SESSION_FILE)
             if not L.context.is_logged_in:
                 logging.info("Logging in...")
-                L.context.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
-                logging.info("Logging Done")
-                with open(SESSION_FILE, 'wb') as session_file:
-                    L.context.save_session_to_file(session_file)
-                    logging.info("logged the session")
+                try:
+                    L.context.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
+                    logging.info("Login successful")
+                    with open(SESSION_FILE, 'wb') as session_file:
+                        L.context.save_session_to_file(session_file)
+                        logging.info("Session saved")
+                except instaloader.exceptions.InstaloaderException as e:
+                    logging.error(f"Login error: {e}")
         else:
             L.context.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
-            with open(SESSION_FILE, 'wb') as session_file:
-                L.context.save_session_to_file(session_file)
-    except instaloader.exceptions.QueryReturnedNotFoundException as e:
-        logging.error(f"QueryNotFoundException: {e}")
-
+            L.save_session_to_file(SESSION_FILE)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
     proxies = {
-        'http': 'socks5://yoqytafd-6:2dng483b96qx@p.webshare.io:80',
-        'https': 'socks5://yoqytafd-6:2dng483b96qx@p.webshare.io:80',
+        'http': HTTP_PROXY,
+        'https': HTTPS_PROXY,
     }
     L.context._session.proxies.update(proxies)
-
     return L
 
-def create_instaloader_instance1():
-    USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    INSTAGRAM_USERNAME = "loopstar154"
-    INSTAGRAM_PASSWORD = "Starbuzz4@"
-
-    L1 = instaloader.Instaloader()
-    
-    L1.user_agent = USER_AGENT
-
-    try:
-        proxies = {
-            'http': 'socks5://yoqytafd-6:2dng483b96qx@p.webshare.io:80',
-            'https': 'socks5://yoqytafd-6:2dng483b96qx@p.webshare.io:80',
-        }
-        L1.context._session.proxies.update(proxies)
-
-        L1.context.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
-
-        L1.save_session_to_file('loopstar154_session4')
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-
 def calculate_engagement_rate(username, last_n_posts=10):
-    # L = create_instaloader_instance()
+    L = create_instaloader_instance()
     profile = instaloader.Profile.from_username(L.context, username)
     all_posts = list(profile.get_posts())
     total_number_of_posts = len(all_posts)
@@ -102,7 +68,29 @@ def calculate_engagement_rate(username, last_n_posts=10):
         else:
             engagement_rate = (total_interactions / number_post) / profile.followers * 100
 
+    time.sleep(random.uniform(1, 3))
     return engagement_rate
+
+# def calculate_engagement_rate(username, last_n_posts=10):
+#     L = create_instaloader_instance()
+#     profile = instaloader.Profile.from_username(L.context, username)
+#     posts = profile.get_posts()
+
+#     total_likes = total_comments = 0
+#     for post in posts:
+#         if last_n_posts <= 0:
+#             break
+#         total_likes += post.likes
+#         total_comments += post.comments
+#         last_n_posts -= 1
+#         time.sleep(random.uniform(1, 3))  # Delay to mimic human behavior and respect rate limits
+
+#     total_interactions = total_likes + total_comments
+#     number_of_posts = profile.mediacount
+
+#     engagement_rate = (total_interactions / 10) / profile.followers * 100
+#     return engagement_rate
+
 
 def extract_phone_number(bio):
     phone_pattern = re.compile(r'\b\d+[-.\s]?\d+[-.\s]?\d+\b')
@@ -116,10 +104,11 @@ def extract_email(bio):
     email_matches = re.findall(email_pattern, bio)
     return email_matches[0] if email_matches else None
 
+
 @app.route('/profile/<username>')
 def get_instagram_profile(username):
+    L = instaloader.Instaloader()
     try:
-        L = create_instaloader_instance()
         profile = instaloader.Profile.from_username(L.context, username)
         engagement_rate = round(calculate_engagement_rate(username), 2)
         
@@ -144,25 +133,13 @@ def get_instagram_profile(username):
         }
         json_data = json.dumps(response, ensure_ascii=False)
         return Response(json_data, content_type='application/json; charset=utf-8')
-
+    
     except instaloader.exceptions.ProfileNotExistsException:
-        response = {
-            'success': False,
-            'message': 'Profile not found',
-            'data': None
-        }
-        return jsonify(response)
+        return jsonify({'success': False, 'message': 'Profile not found', 'data': None})
 
     except Exception as e:
-        response = {
-            'success': False,
-            'message': f"An error occurred while fetching profile: {e}",
-            'data': None
-        }
-        if response:
-            create_instaloader_instance1()
-        return jsonify(response)
-
+        logging.error(f"An error occurred while fetching profile: {e}")
+        return jsonify({'success': False, 'message': str(e), 'data': None})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
